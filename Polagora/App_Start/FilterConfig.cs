ac087cc0f25a7ea4d77c76c1adda40db2f69eac6
A G Smith <aglensmith@gmail.com>
2016-02-21 17:48:30 -0600

    commented out filters.Add(new HandleErrorAttribute());

--- Polagora/App_Start/FilterConfig.cs
+++ Polagora/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace Polagora
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
     }
 }
