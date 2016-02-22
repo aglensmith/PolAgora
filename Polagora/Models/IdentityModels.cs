bb0345c707396555b5d814132cc5ab361d6c19dc
A G Smith <aglensmith@gmail.com>
2016-02-21 18:41:36 -0600

    Changed DefaultContext to PolagoraContext

--- Polagora/Models/IdentityModels.cs
+++ Polagora/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Polagora.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("PolagoraContext", throwIfV1Schema: false)
         {
         }
 
