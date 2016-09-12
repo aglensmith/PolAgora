6b024afb326d4344403b27376e6122534c0d93e4
Austin <aglensmith@gmail.com>
2016-09-11 17:17:13 -0700

    commit to push to desktop

---
+++ PolagoraWebJob/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Polagora.Models;
+using Polagora.UtilityClasses;
+using ApiCaller;
+using System.Configuration;
+using System.Threading;
+
+namespace PolagoraWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new System.Threading.Timer(
+                e => MainAsync().Wait(),
+                null,
+                0,
+                TimeSpanMinutesConverter(5),
+                TimeSpanMinutesConverter
+        }
+
+        static async Task MainAsync()
+        {
+            //add try catch
+            SnapshotManager SnapshotManager = new SnapshotManager() { DBContext = new PolagoraContext() };
+
+            //Call Twitter and Facebook
+            string FacebookToken = ConfigurationManager.AppSettings["FacebookToken"];
+            string TwitterToken = ConfigurationManager.AppSettings["TwitterBearer"];
+            Dictionary<string, FacebookCaller.FacebookResponse> 
+                FacebookResponses = await FacebookCaller.CallFacebookAsync(SnapshotManager.FacebookIDs, FacebookToken);
+            Dictionary<string, TwitterCaller.TwitterResponse> 
+                TwitterResponses = await TwitterCaller.CallTwitterAsync(SnapshotManager.TwitterIDs, TwitterToken);
+
+            //Insert Snapshots into DB
+            SnapshotManager.InsertSnapshots(FacebookResponses, TwitterResponses);
+        }
+
+    }
+}
